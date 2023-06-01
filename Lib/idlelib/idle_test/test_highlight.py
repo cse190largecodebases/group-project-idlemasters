ccbe4ff861de88a231c3cd840a78eea7410daee3
Chengcheng Zhang <chz009@ucsd.edu>
2023-05-31 21:53:56 -0700

    bug needs to be fixed

--- Lib/idlelib/idle_test/test_highlight.py
+++ Lib/idlelib/idle_test/test_highlight.py
@@ -1,44 +1,39 @@
 import unittest
+from unittest.mock import MagicMock
 from tkinter import Tk
 from idlelib.highlight import HighlightParagraph
 from idlelib.idle_test.mock_idle import Editor, Func
-
-class MockText:
-    def __init__(self):
-        self.index_calls = []
-
-    def index(self, index):
-        self.index_calls.append(index)
-        line_number = int(index.split('.')[0])
-        next_line_number = line_number + 1
-        return f"{next_line_number}.0"
-
-
-class Editor:
-    def __init__(self, root):
-        self.text = MockText()
-        self.allow_highlight = True
-        self.menudict = {}
-        self.color_menu = {}
-
+from idlelib.idle_test.mock_tk import Text
 
 class HighlightParagraphTestCase(unittest.TestCase):
     def setUp(self):
-        self.root = Tk()
-        self.editor = Editor(self.root)
+        self.root = Tk()  # Create a Tkinter root window
+        text = Text()
+        multiline_test_string = (
+        "    '''The first line is under the max width.\n"
+        "    The second line's length is way over the max width. It goes "
+        "on and on until it is over 100 characters long.\n"
+        "    Same thing with the third line. It is also way over the max "
+        "width, but FormatParagraph will fix it.\n"
+        "    '''\n")
+        text.insert('1.0', multiline_test_string)
+        self.editor = Editor(self.root, text=text)
         self.highlighter = HighlightParagraph(self.editor)
         self.editor.menudict = {'edit': Func(return_self=True)}
         self.editor.color_menu = Func(return_self=True)
 
     def tearDown(self):
-        self.root.destroy()
+        self.root.destroy()  # Destroy the Tkinter root window
 
     def test_toggle_highlight(self):
         start, end = '1.0', '2.0'
         color = "light blue"
         self.editor.get_selection_indices = Func(result=(start, end))
+        self.editor.text.tag_names = Func(result=[])
+        self.editor.text.index = Func(return_self=True)
+        print("1")
         self.highlighter.toggle_highlight(color)
-        self.assertEqual(self.editor.text.index_calls, ['1.0', '2.0', '3.0'])  # Verify the index calls
+        print("2")
         self.assertEqual(self.editor.text.tag_add.called, 1)
         self.assertEqual(self.editor.text.tag_config.called, 1)
 
